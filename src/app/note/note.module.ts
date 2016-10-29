5757117227e733f9941dbc5daf4104fbc1eb77fc
Paweł Tomaszewski <pawel.tomaszewski@dreamlab.pl>
2016-10-29 14:14:23 +0200

    created note module

---
+++ src/app/note/note.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NoteComponent } from './note.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [NoteComponent]
+})
+export class NoteModule { }
