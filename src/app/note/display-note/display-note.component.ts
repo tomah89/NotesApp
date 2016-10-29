10d4ee10e9b77aa39ea565d99bce9af78873d99c
Paweł Tomaszewski <pawel.tomaszewski@dreamlab.pl>
2016-10-29 15:41:35 +0200

    attached display note

--- src/app/note/display-note/display-note.component.ts
+++ src/app/note/display-note/display-note.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-display-note',
+  selector: 'display-note',
   templateUrl: './display-note.component.html',
   styleUrls: ['./display-note.component.css']
 })
 export class DisplayNoteComponent implements OnInit {
 
+  public notes = [];
+
   constructor() { }
 
   ngOnInit() {
+    this.notes = [
+      {
+        subject: "Test",
+        body: "Test message"
+      }
+    ]
   }
 
 }
